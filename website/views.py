957faeeb571206c3aadb3c708f84e82b4432299a
LouisI299 <louisingelbrecht@gmail.com>
2024-04-30 21:58:49 +0200

    Filters aangepast

--- website/views.py
+++ website/views.py
@@ -7,37 +7,26 @@ from datetime import datetime
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET', 'PUT', 'POST'])
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if (request.method == 'PUT'):
+    if request.method == 'POST':
         sortItems = request.form.get('AZ')
         category = request.form.get('category')
-        if sortItems == 'az':
-            artikels = Artikel.query.order_by(Artikel.title).all()
-            return render_template("home.html", user=current_user, artikels = artikels)
-        elif sortItems == 'za':
-            artikels = Artikel.query.order_by(Artikel.title.desc()).all()
-            return render_template("home.html", user=current_user, artikels = artikels)
-        if category == 'all':
-            artikels = Artikel.query.all()
-            return render_template("home.html", user=current_user, artikels = artikels)
-        elif category == 'audio':
-            artikels = Artikel.query.filter_by(category = 'Audio').all()
-            return render_template("home.html", user=current_user, artikels = artikels)
-        elif category == 'video':
-            artikels = Artikel.query.filter_by(category = 'Video').all()
-            return render_template("home.html", user=current_user, artikels = artikels)
-        elif category == 'varia':
-            artikels = Artikel.query.filter_by(category = 'Varia').all()
-            return render_template("home.html", user=current_user, artikels = artikels)
-        elif category == 'belichting':
-            artikels = Artikel.query.filter_by(category = 'Belichting').all()
-            return render_template("home.html", user=current_user, artikels = artikels)
-        elif category == 'xr':
-            artikels = Artikel.query.filter_by(category = 'XR').all()
-            return render_template("home.html", user=current_user, artikels = artikels)
-        
+
+        if category == 'All':
+            query = Artikel.query
+        else:
+            query = Artikel.query.filter_by(category=category)
+
+        if sortItems == 'AZ':
+            artikels = query.order_by(Artikel.title).all()
+        elif sortItems == 'ZA':
+            artikels = query.order_by(Artikel.title.desc()).all()
+        else:
+            artikels = query.all()
+
+        return render_template("home.html", user=current_user, artikels=artikels)
     if (request.method == 'POST'): 
         datums = request.form.get('datepicker').split(' to ')
         artikelid = request.form.get('artikel_id')
@@ -47,8 +36,6 @@ def home():
             eindDatum = datetime.strptime(datums[1], '%Y-%m-%d')
             if startDatum.weekday() >= 5 or eindDatum.weekday() >= 5:
                 raise ValueError('Reservatie is niet toegestaan op zaterdag of zondag')
-            if current_user.type_id == 2 and (startDatum.weekday() == 4 or eindDatum.weekday() == 4):
-                raise ValueError('Reservatie is niet toegestaan voor studenten')
             elif current_user.type_id == 2 and (eindDatum - startDatum).days > 7:
                 raise ValueError('Reservatie is niet toegestaan voor studenten langer dan 7 dagen')
             new_uitlening = Uitlening(user_id = current_user.id, artikel_id = artikelid, start_date = startDatum, end_date = eindDatum)
