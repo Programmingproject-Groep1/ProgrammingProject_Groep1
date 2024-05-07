1600a063433eeb26815237eaed80aee6186cb647
LouisI299 <louisingelbrecht@gmail.com>
2024-05-07 12:17:51 +0200

    link in nav naar blacklist

--- website/views.py
+++ website/views.py
@@ -118,10 +118,10 @@ def home():
 
 
 #Blacklist pagina admin
-@views.route('/blacklist', methods=['GET', 'POST'])
+@views.route('/adminblacklist', methods=['GET', 'POST'])
 def blacklist():
     users = User.query 
-    return render_template("adminblacklist.html", user=current_user)
+    return render_template("adminblacklist.html", user=current_user, users=users)
 #Zorgt ervoor dat images geladen kunnen worden
 @views.route('images/<path:filename>')
 def get_image(filename):
