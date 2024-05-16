79299c6168ec42a4ef39182eb6994b7844d15230
milanvt <milanvt18@gmail.com>
2024-05-16 11:18:20 +0200

    backend gedeelte van de textarea

--- website/views.py
+++ website/views.py
@@ -238,13 +238,14 @@ def admin_blacklist():
             # kijken of de gebruiker wordt gebanned
             if request.form.get('form_name') == 'ban':
                 user_id = request.form.get('userid')
+                reden_blacklist = request.form.get('reden_blacklist')
                 user = User.query.get(user_id)
                 if user:
                     user.blacklisted = True
+                    user.reden_blacklist = reden_blacklist
                     # laat de geruiker gebanned worden voor 3 maanden
                     user.blacklist_end_date = datetime.now() + timedelta(days=90)
                     #melding meegeven
-                    
                     db.session.commit()
                     flash('Gebruiker verbannen voor 3 maanden.', category='success')
                 else:
