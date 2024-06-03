be921b880cd0cb589b932c06788938e815ba4803
haytoche <haytham.tahere@student.ehb.be>
2024-06-03 15:22:03 +0100

    signup weggehaald

--- website/auth.py
+++ website/auth.py
@@ -40,40 +40,40 @@ def logout():
 
 
 #Account aanmaken
-@auth.route('/sign-up', methods=['GET', 'POST'])
-def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-        userType = request.form.get('userType')
-        if check_input(email) == False or check_input(first_name) == False or check_input(password1) == False or check_input(password2) == False:
-            return render_template("signup.html", user=current_user)
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists.', category='modalerror')
-        elif len(email) < 4:
-            flash('Email must be greater than 4 characters', category='modalerror')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character', category='modalerror')
-        elif password1 != password2:
-            flash('Passwords don\'t match', category='error')
-        elif len(password1) < 7:
-            flash('Password must be greater than 7 characters', category='modalerror')
-        else:
-            try:
-                new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256', salt_length= 16), type_id=userType)
-                db.session.add(new_user)
-                db.session.commit()
-                login_user(new_user, remember=True)
-                flash('Account created!', category='modal')
-                return redirect(url_for('views.home'))
-            except Exception as e:
-                flash(f'Error creating account: {str(e)}', category='modalerror')
-                db.session.rollback()  # Rollback changes if an error occurs
+# @auth.route('/sign-up', methods=['GET', 'POST'])
+# def sign_up():
+#     if request.method == 'POST':
+#         email = request.form.get('email')
+#         first_name = request.form.get('firstName')
+#         password1 = request.form.get('password1')
+#         password2 = request.form.get('password2')
+#         userType = request.form.get('userType')
+#         if check_input(email) == False or check_input(first_name) == False or check_input(password1) == False or check_input(password2) == False:
+#             return render_template("signup.html", user=current_user)
+#         user = User.query.filter_by(email=email).first()
+#         if user:
+#             flash('Email already exists.', category='modalerror')
+#         elif len(email) < 4:
+#             flash('Email must be greater than 4 characters', category='modalerror')
+#         elif len(first_name) < 2:
+#             flash('First name must be greater than 1 character', category='modalerror')
+#         elif password1 != password2:
+#             flash('Passwords don\'t match', category='error')
+#         elif len(password1) < 7:
+#             flash('Password must be greater than 7 characters', category='modalerror')
+#         else:
+#             try:
+#                 new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256', salt_length= 16), type_id=userType)
+#                 db.session.add(new_user)
+#                 db.session.commit()
+#                 login_user(new_user, remember=True)
+#                 flash('Account created!', category='modal')
+#                 return redirect(url_for('views.home'))
+#             except Exception as e:
+#                 flash(f'Error creating account: {str(e)}', category='modalerror')
+#                 db.session.rollback()  # Rollback changes if an error occurs
 
             
            
 
-    return render_template("signup.html", user=current_user)
+#     return render_template("signup.html", user=current_user)
